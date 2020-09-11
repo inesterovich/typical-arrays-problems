8f4d86ccd26e9b1cb7330b30703873d4784ee640
Ilya Nesterovich <manager@inesterovich.ru>
2020-09-11 16:52:10 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+  if ( array === undefined || array.length === 0 ) {
+    return 0; 
+  } else {
+
+    const max = array.reduce((sum, current) => sum + current, 0);
+    const avg = max/array.length;
+
+    return avg;
+
+  }
+ 
+
 }
